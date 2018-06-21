353f4a09c6334375866ab64b984bd01c2f17b8f3
cloudera <cloudera@quickstart.cloudera>
2018-06-20 18:14:56 -0700

    test push to git

--- W02-SparkApplication/src/simple/SparkContextExample.scala
+++ W02-SparkApplication/src/simple/SparkContextExample.scala
@@ -5,8 +5,8 @@ import org.apache.spark.SparkContext
 
 object SparkContextExample {
    def main(args: Array[String]) {
-    val stocksPath = "hdfs://quickstart.cloudera/user/cloudera/stocks.txt"  //read from HDFS
-    //val stocksPath = "/home/cloudera/git/S-BEAD/W02-SparkApplication/data/stocks.txt" //read from local file
+    //val stocksPath = "hdfs://quickstart.cloudera/user/cloudera/stocks.txt"  //read from HDFS
+    val stocksPath = "/home/cloudera/git/S-BEAD/W02-SparkApplication/data/stocks.txt" //read from local file
     val conf = new SparkConf().setAppName("Counting Lines").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val data = sc.textFile(stocksPath, 3) //chop into n jobs
